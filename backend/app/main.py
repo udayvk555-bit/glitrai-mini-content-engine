f1dd930e5ed801c69485430b399d02c2f086a451
UDAY Vk <udayvk555@gmail.com>
2026-07-29 19:24:37 +0530

    Fix CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,7 +10,8 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500"],
+    allow_origins=["https://glitrai-mini-content-engine-chi.vercel.app"],
+
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
